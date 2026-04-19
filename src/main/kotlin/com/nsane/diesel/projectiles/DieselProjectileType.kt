1bb062a505d05a04d0cedc0cbe668be86a7b1511
gardens <91749954+glassgardens@users.noreply.github.com>
2026-04-19 07:16:50 -0400

    rearramge some shit start testing shotgun

--- src/main/kotlin/com/nsane/diesel/projectiles/DieselProjectileType.kt
+++ src/main/kotlin/com/nsane/diesel/projectiles/DieselProjectileType.kt
@@ -11,9 +11,20 @@ enum class DieselProjectileType(
     val bulletSpeed: Double = 25.0,
     val bulletDistance: Double = 100.0,
 ) {
-    SCOUT_GUN("Projectile_AA_Asset", 10, 7.0,  2.0f, 40.0, 8.0),
-    AA("Projectile_AA_Asset", 1, 2.0, 10.0f),
-    FLAK("Projectile_AA_Asset", 1, 0.5, 50.0f);
+    SHOTGUN_SCOUT("Bullet_Blunderbuss",
+        10,
+        7.0,
+        2.0f,
+        40.0,
+        8.0),
+    TURRET_AA("Projectile_AA_Asset",
+        1,
+        2.0,
+        10.0f),
+    TURRET_FLAK("Projectile_AA_Asset",
+        1,
+        0.5,
+        50.0f);
 
     val model get() = ModelAsset.getAssetMap().getAsset(modelKey) ?: throw Exception("Model $modelKey not found")
 
