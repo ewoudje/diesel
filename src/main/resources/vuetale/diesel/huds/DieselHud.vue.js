6d419a510fddfe80f06e303c16654609508274b9
gardens <91749954+glassgardens@users.noreply.github.com>
2026-04-24 16:59:50 -0400

    a whole lot

--- src/main/resources/vuetale/diesel/huds/DieselHud.vue.js
+++ src/main/resources/vuetale/diesel/huds/DieselHud.vue.js
@@ -1,85 +1,123 @@
-import { defineComponent as h, ref as r, openBlock as _, createElementBlock as f, Fragment as x, createElementVNode as t, toDisplayString as n, unref as B } from "vue";
+import { defineComponent as _, ref as n, openBlock as u, createElementBlock as g, Fragment as p, createElementVNode as t, toDisplayString as a, renderList as b } from "vue";
 import "vt:@core/components/Common";
 import "vt:@core/components/core";
-import { useData as v } from "vt:@core/composables/useData";
-const T = { anchor: { Left: -20, Bottom: -20 } }, y = {
-  anchor: { Left: 250, Bottom: 0, Width: 700, Height: 200 },
-  background: "Common/DropdownBox.png"
-}, F = ["el-style"], b = { anchor: { Right: -20, Bottom: -20 } }, D = {
-  anchor: { Width: 300, Height: 300, Right: 0, Bottom: 0 },
-  background: "Common/DropdownBox.png"
-}, C = { anchor: { Top: 20, Left: 5 } }, H = ["el-style"], R = ["el-style"], W = {
-  anchor: { Top: -20, Right: -20, Width: 800, Height: 120 },
-  background: "Common/DropdownBox.png"
-}, k = ["el-style"], a = /* @__PURE__ */ h({
+import { useData as k } from "vt:@core/composables/useData";
+const f = { anchor: { Left: 0, Top: 0 } }, v = ["background"], G = ["background"], I = ["background"], T = { anchor: { Left: -20, Bottom: -50 } }, y = { anchor: { Left: -30, Bottom: 0, Width: 1e3, Height: 440 } }, H = ["background"], x = ["background"], W = ["el-style"], L = { anchor: { Top: 0, Right: -35 } }, R = ["background"], B = ["el-style"], F = { anchor: { Right: 0 } }, E = ["background"], C = ["background"], N = ["background"], P = ["background"], S = { anchor: { Right: -20, Bottom: -52 } }, U = ["background"], A = ["background"], D = ["background"], M = { anchor: { Top: 10, Left: 40 } }, V = ["el-style"], z = ["el-style"], i = /* @__PURE__ */ _({
   __name: "DieselHud",
-  setup(p) {
-    console.log("INIT VUE HUDs");
-    const d = r({
-      actor: "peeb",
-      text: "not fast enough! chop chop!",
-      voice: "/file/path"
-    });
-    r(5), r("");
-    const l = {
-      ammo: 6,
-      maxAmmo: 120
-    }, m = r({
-      text: "breach the paalais"
-    });
-    r("");
-    const e = r({
+  setup(h) {
+    console.log("[DIESELGAME] INIT VUE HUD");
+    const e = n({
       textPrimary: "#FB0004",
       textSecondary: "#fc7b03",
       activeTextPrimary: "#FB0004"
-    }), c = v("test", "default value");
-    setTimeout(() => {
-      u();
-    }, 500), setInterval(() => {
+    }), m = n({
+      actor: "peeb",
+      text: "not fast enough! chop chop!",
+      voice: "/file/path"
+    }), r = n("Img/empty.png");
+    n("");
+    const d = {
+      ammo: 6,
+      maxAmmo: 120
+    }, s = n({
+      text: "breach the palais"
     });
-    let s = !1;
-    function u() {
-      s = !0;
-      const i = [
-        "#FB0004",
-        "#DF2E00",
-        "#FB0004",
-        "#AF3D00",
-        "#9A3D00"
-      ];
-      let o = 0;
-      g();
-      function g() {
-        o++, o >= i.length - 1 && (e.value.activeTextPrimary = e.value.textPrimary, o = 0), i[o] && (e.value.activeTextPrimary = i[o]);
+    n("");
+    const c = n([
+      {
+        name: "1",
+        active: !0,
+        type: "item",
+        detailImage: "",
+        color: null
+      },
+      {
+        name: "2",
+        active: !1,
+        type: "item",
+        detailImage: "",
+        color: null
+      },
+      {
+        name: "3",
+        active: !1,
+        type: "ability",
+        detailImage: "",
+        color: null
+      },
+      {
+        name: "4",
+        active: !1,
+        type: "ability",
+        detailImage: "",
+        color: null
       }
-    }
-    return (i, o) => (_(), f(x, null, [
-      o[2] || (o[2] = t("Group", {
-        anchor: { Left: -20, Top: -20, Width: 350, Height: 500 },
-        background: "Common/DropdownBox.png"
-      }, [
-        t("ProgressBar", {
-          anchor: { Top: 0, Left: 0, Width: 350, Height: 500 },
-          background: "Img/progress_back.png",
-          "bar-texture-path": "Img/progress_front.png",
-          "effect-texture-path": "Img/progress_effect.png",
-          "effect-height": 100,
-          "effect-width": 100,
-          value: 0.5,
-          alignment: "Vertical",
-          direction: "Start"
-        }),
-        t("Group", { background: "Common/DropdownBox.png" }),
-        t("Group", { background: "Common/DropdownBox.png" })
-      ], -1)),
-      o[3] || (o[3] = t("Group", {
-        anchor: { Right: -20, Top: 0, Bottom: 0, Width: 120 },
-        background: "Common/DropdownBox.png"
+    ]);
+    return k("test", "default value"), setTimeout(() => {
+    }, 500), setInterval(() => {
+    }), (w, o) => (u(), g(p, null, [
+      o[9] || (o[9] = t("Group", {
+        anchor: { Left: -50, Bottom: -35, Top: -25, Width: 200, Height: 440 },
+        background: "Img/left.png"
       }, null, -1)),
+      t("Group", f, [
+        o[0] || (o[0] = t("Group", { anchor: { Left: -100, Top: -90, Width: 640, Height: 500 } }, [
+          t("Group", { background: "Img/top_left.png" })
+        ], -1)),
+        t("Group", {
+          anchor: { Left: 0, Top: 0, Width: 340, Height: 380 },
+          background: { Color: e.value.activeTextPrimary },
+          "mask-texture-path": "Img/health/scout1.png"
+        }, null, 8, v),
+        t("Group", {
+          anchor: { Left: -20, Top: -20, Width: 350, Height: 500 },
+          background: r.value
+        }, [
+          t("Group", { background: r.value }, null, 8, I)
+        ], 8, G),
+        o[1] || (o[1] = t("Group", {
+          anchor: { Left: -100, Top: -90, Width: 640, Height: 500 },
+          background: "Img/top_left_shine.png"
+        }, null, -1))
+      ]),
       t("Group", T, [
         t("Group", y, [
+          o[2] || (o[2] = t("Group", { background: "Img/bottom_left_wide.png" }, null, -1)),
+          t("Group", {
+            anchor: { Left: 60, Bottom: 22, Width: 320, Height: 320 },
+            background: { Color: e.value.activeTextPrimary },
+            "mask-texture-path": "Img/portrait/prole1.png"
+          }, null, 8, H)
+        ]),
+        t("Group", {
+          anchor: { Left: 400, Bottom: 100, Width: 530, Height: 100 },
+          background: r.value
+        }, [
           t("Label", {
-            anchor: { Left: 50, Width: 600 },
+            "el-style": {
+              FontSize: 35,
+              FontName: "Secondary",
+              RenderUppercase: !0,
+              RenderBold: !0,
+              Wrap: !0,
+              TextColor: e.value.activeTextPrimary
+            }
+          }, a(m.value.text), 9, W)
+        ], 8, x),
+        o[3] || (o[3] = t("Group", {
+          anchor: { Left: -30, Bottom: 0, Width: 1e3, Height: 440 },
+          background: "Img/bottom_left_wide_shine.png"
+        }, null, -1))
+      ]),
+      t("Group", L, [
+        o[4] || (o[4] = t("Group", { anchor: { Top: 0, Right: 80, Width: 700, Height: 220 } }, [
+          t("Group", { background: "Img/top_right.png" })
+        ], -1)),
+        t("Group", {
+          anchor: { Top: 10, Right: 170, Width: 580, Height: 90 },
+          background: r.value
+        }, [
+          t("Label", {
             padding: { Full: 20 },
             "el-style": {
               FontSize: 43,
@@ -89,68 +127,95 @@ const T = { anchor: { Left: -20, Bottom: -20 } }, y = {
               Wrap: !0,
               TextColor: e.value.activeTextPrimary
             }
-          }, n(d.value.text) + " " + n(B(c)), 9, F)
-        ]),
-        o[0] || (o[0] = t("Group", {
-          anchor: { Left: 0, Bottom: 0, Width: 300, Height: 300 },
-          background: "Common/DropdownBox.png"
+          }, a(s.value.text), 9, B)
+        ], 8, R),
+        o[5] || (o[5] = t("Group", {
+          anchor: { Top: 0, Right: 80, Width: 700, Height: 220 },
+          background: "Img/top_right_shine.png"
         }, null, -1))
       ]),
-      t("Group", b, [
-        o[1] || (o[1] = t("Group", {
-          anchor: { Right: 250, Bottom: 0, Width: 400, Height: 150 },
-          background: "Common/DropdownBox.png"
+      t("Group", F, [
+        o[6] || (o[6] = t("Group", {
+          anchor: { Right: -180, Top: -10, Bottom: 1, Width: 320 },
+          background: "Img/right.png"
         }, null, -1)),
-        t("Group", D, [
-          t("Group", C, [
+        t("Group", {
+          anchor: { Right: 0, Top: 0, Bottom: 0, Width: 120 },
+          background: r.value,
+          "layout-mode": "Top"
+        }, [
+          (u(!0), g(p, null, b(c.value, (l) => (u(), g("Group", {
+            anchor: { Left: 0, Top: 22, Width: 100, Height: 100 },
+            background: l.active ? "Img/button_on_overlay.png" : "Img/button_off_overlay.png",
+            "mask-texture-path": "Img/masks/button.png"
+          }, [
+            t("Group", {
+              background: { Color: `${e.value.activeTextPrimary}(0.5)` }
+            }, null, 8, N),
+            t("Group", {
+              background: l.active ? "Img/button_on_top_overlay.png" : "Img/empty.png"
+            }, null, 8, P),
+            t("Label", null, a(l.name), 1)
+          ], 8, C))), 256))
+        ], 8, E)
+      ]),
+      t("Group", S, [
+        o[7] || (o[7] = t("Group", { anchor: { Bottom: 0, Right: -90, Width: 800, Height: 450 } }, [
+          t("Group", { background: "Img/bottom_right.png" })
+        ], -1)),
+        t("Group", {
+          anchor: { Right: 250, Bottom: 0, Width: 400, Height: 150 },
+          background: r.value
+        }, [
+          t("Group", {
+            anchor: { Left: -20, Bottom: 62, Width: 400, Height: 130 },
+            background: { Color: e.value.activeTextPrimary },
+            "mask-texture-path": "Img/masks/bottom_right_mask.png"
+          }, null, 8, A)
+        ], 8, U),
+        t("Group", {
+          anchor: { Width: 300, Height: 300, Right: 33, Bottom: 30 },
+          background: r.value
+        }, [
+          t("Group", M, [
             t("Label", {
               anchor: { Top: 10, Left: 0, Width: 300, Height: 200 },
               padding: { Full: 20 },
               "el-style": {
-                FontSize: 210,
+                FontSize: 170,
                 FontName: "Default",
                 HorizontalAlignment: "Start",
                 VerticalAlignment: "Center",
                 Wrap: !1,
                 TextColor: e.value.activeTextPrimary
               }
-            }, n(l.ammo), 9, H),
+            }, a(d.ammo), 9, V),
             t("Label", {
-              anchor: { Top: 100, Left: 80, Width: 200, Height: 200 },
+              anchor: { Top: 100, Left: 10, Width: 200, Height: 200 },
               padding: { Full: 20 },
               "el-style": {
-                FontSize: 60,
+                FontSize: 50,
                 FontName: "Default",
                 HorizontalAlignment: "End",
                 VerticalAlignment: "Center",
                 Wrap: !1,
                 TextColor: e.value.activeTextPrimary
               }
-            }, n(l.maxAmmo), 9, R)
+            }, a(d.maxAmmo), 9, z)
           ])
-        ])
+        ], 8, D),
+        o[8] || (o[8] = t("Group", {
+          anchor: { Bottom: 0, Right: -90, Width: 800, Height: 450 },
+          background: "Img/bottom_right_shine.png"
+        }, null, -1))
       ]),
-      t("Group", W, [
-        t("Label", {
-          anchor: { Top: 20, Width: 600, Left: 20 },
-          padding: { Full: 20 },
-          "el-style": {
-            FontSize: 43,
-            FontName: "Secondary",
-            RenderUppercase: !0,
-            RenderBold: !0,
-            Wrap: !0,
-            TextColor: e.value.activeTextPrimary
-          }
-        }, n(m.value.text), 9, k)
-      ]),
-      o[4] || (o[4] = t("Group", { anchor: { Full: 1 } }, null, -1))
+      o[10] || (o[10] = t("Group", { anchor: { Full: 1 } }, null, -1))
     ], 64));
   }
 });
-a.__hmrId = "22684cca";
-typeof __VUE_HMR_RUNTIME__ < "u" && (__VUE_HMR_RUNTIME__.createRecord(a.__hmrId, a) || __VUE_HMR_RUNTIME__.reload(a.__hmrId, a));
+i.__hmrId = "22684cca";
+typeof __VUE_HMR_RUNTIME__ < "u" && (__VUE_HMR_RUNTIME__.createRecord(i.__hmrId, i) || __VUE_HMR_RUNTIME__.reload(i.__hmrId, i));
 export {
-  a as default
+  i as default
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRGllc2VsSHVkLnZ1ZS5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRGllc2VsSHVkLnZ1ZS5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
--- src/vue/lib/huds/DieselHud.vue
+++ src/vue/lib/huds/DieselHud.vue
@@ -4,44 +4,72 @@ import { Core } from 'vt:@core/components/core';
 import { useData } from 'vt:@core/composables/useData';
 import { onMounted, ref } from 'vue';
 import { testfn, peeb } from './diesel'
-
+console.log("[DIESELGAME] INIT VUE HUD")
 //dummy args
 
-
-
-console.log("INIT VUE HUDs")
-
-//console.log(gsap)
+//UI Colors
+const colors = ref({
+    textPrimary: '#FB0004',
+    textSecondary: '#fc7b03',
+    activeTextPrimary: '#FB0004'
+})
+//Current message
 const message = ref({
     actor:'peeb',
     text: `not fast enough! chop chop!`,
     voice: `/file/path`
 })
-
-const ammo = ref(5);
+//Debug background
+const contentBg = ref('Img/empty.png')
+//Weapons
 const heldWeapon = ref<string>('');
 const weapon = {
     ammo: 6,
     maxAmmo: 120,
     image: '/file/path'
 }
+//Current objective
 const objective = ref({
-    text: 'breach the paalais'
+    text: 'breach the palais'
 });
+//Player class
 const playerClass = ref<string>('');
+//Hotbar
+const hotbar = ref([
+    {
+        name: '1',
+        active:true,
+        type: 'item',
+        detailImage: '',
+        color:null
+    },
+        {
+        name: '2',
+        active:false,
+        type: 'item',
+        detailImage: '',
+        color:null
+    },
+        {
+        name: '3',
+        active:false,
+        type: 'ability',
+        detailImage: '',
+        color:null
+    },
+        {
+        name: '4',
+        active:false,
+        type: 'ability',
+        detailImage: '',
+        color:null
+    }
+])
 
-const colors = ref({
-    textPrimary: '#FB0004',
-    textSecondary: '#fc7b03',
-    activeTextPrimary: '#FB0004'
-})
 
 const test = useData<string>("test", "default value")
 setTimeout(()=>{
-    
-    damageFlicker();
-    
-    
+    //damageFlicker();
 },500)
 let a = 0
 setInterval(()=>{
@@ -75,71 +103,136 @@ function damageFlicker(){
 <template>
     <!--Honestly I'm lazy and it's not like this is getting reused anywhere so who needs components really-->
 
-    <!--Health-->
-    <Group :anchor="{  Left: -20, Top: -20, Width: 350, Height: 500 }" :background="'Common/DropdownBox.png'">
-        <!--Underlying progress bar-->
-        <ProgressBar
-            :anchor="{Top:0, Left:0, Width: 350, Height: 500}"
-            :background="'Img/progress_back.png'"
-            :bar-texture-path="'Img/progress_front.png'"
-            :effect-texture-path="'Img/progress_effect.png'"
-            :effect-height="100"
-            :effect-width="100"
-            :value="0.5"
-            :alignment="'Vertical'"
-            :direction="'Start'"
-        ></ProgressBar>
-        <!--3d model-->
-        <Group :background="'Common/DropdownBox.png'">
+    <!--===MISC UNDERLAYS===-->
+    <Group :anchor="{ Left:-50,Bottom:-35,Top:-25,Width: 200, Height: 440 }" :background="'Img/left.png'"/>
+
+
+    <!--==HEALTH===-->
+    <Group :anchor="{Left:0,Top:0}">
+        <!--Image asset-->
+        <Group :anchor="{ Left:-100,Top:-90,Width: 640, Height: 500 }">
+            <Group :background="'Img/top_left.png'"/>
         </Group>
-        <!--Class-based overlay image-->
-        <Group :background="'Common/DropdownBox.png'">
+            <Group :anchor="{Left:0, Top:0, Width: 340, Height: 380 }" :background="{Color:colors.activeTextPrimary}" :mask-texture-path="'Img/health/scout1.png'"></Group>
+
+        <Group :anchor="{  Left: -20, Top: -20, Width: 350, Height: 500 }" :background="contentBg">
+            <!--Underlying progress bar
+            <ProgressBar
+                :anchor="{Top:0, Left:0, Width: 350, Height: 500}"
+                :background="'Img/progress_back.png'"
+                :bar-texture-path="'Img/progress_front.png'"
+                :effect-texture-path="'Img/progress_effect.png'"
+                :effect-height="100"
+                :effect-width="100"
+                :value="0.5"
+                :alignment="'Vertical'"
+                :direction="'Start'"
+            ></ProgressBar>-->
+            <!--3d model-->
+            <!--Class-based overlay image-->
+            <Group :background="contentBg">
+            </Group>
         </Group>
+
+        <!--Shine overlay-->
+        <Group :anchor="{ Left:-100,Top:-90,Width: 640, Height: 500 }" :background="'Img/top_left_shine.png'"/>
     </Group>
+    
 
-    <!--Hotbar-->
-    <Group :anchor="{Right:-20, Top:0, Bottom:0, Width:120}" :background="'Common/DropdownBox.png'">
 
-    </Group>
-
-    <!--Dialogue-->
-    <Group :anchor="{Left:-20, Bottom: -20,}">
+    <!--==DIALOGUE===-->
+    <Group :anchor="{Left:-20, Bottom: -50,}">
+        <!--Image asset-->
+        <Group :anchor="{ Left:-30,Bottom:0,Width: 1000, Height: 440 }">
+            <Group :background="'Img/bottom_left_wide.png'"/>    
+            <!--portrait-->
+            <Group :anchor="{Left:60, Bottom:22, Width: 320, Height: 320 }" :background="{Color:colors.activeTextPrimary}" :mask-texture-path="'Img/portrait/prole1.png'"></Group>
+        </Group> 
+  
         <!--Text-->
-        <Group :anchor="{ Left:250,Bottom:0,Width: 700, Height: 200 }" :background="'Common/DropdownBox.png'">
+        <Group :anchor="{ Left:400,Bottom:100,Width: 530, Height: 100 }" :background="contentBg">
             <Label
-                :anchor="{Left:50, Width:600}"
-                :padding="{Full:20}"
                 :el-style="{
-                    FontSize: 43,
+                    FontSize: 35,
                     FontName: 'Secondary',
                     RenderUppercase:true,
                     RenderBold: true,
                     Wrap: true,
                     TextColor: colors.activeTextPrimary
                 }"
-            >{{ message.text }} {{ test }}</Label>
-        </Group>
-        <!--Portrait-->
-        <Group :anchor="{ Left:0,Bottom:0,Width: 300, Height: 300 }" :background="'Common/DropdownBox.png'">
-
+            >{{ message.text }}</Label>
         </Group>
 
+
+        <!--Shine overlay-->
+        <Group :anchor="{ Left:-30,Bottom:0,Width: 1000, Height: 440 }" :background="'Img/bottom_left_wide_shine.png'"></Group>
     </Group>
 
-    <!--Weapon info-->
-    <Group :anchor="{  Right: -20, Bottom: -20}" >
+
+
+    <!--==OBJECTIVES==-->
+    <Group :anchor="{Top:0,Right:-35}">
+        <!--Image asset-->
+        <Group :anchor="{Top:0,Right:80, Width:700,Height:220}">
+            <Group :background="'Img/top_right.png'"/>
+        </Group>
+        <!--Text-->
+        <Group :anchor="{Top: 10, Right:170, Width:580, Height:90}" :background="contentBg">
+                <Label
+                    :padding="{Full:20}"
+                    :el-style="{
+                        FontSize: 43,
+                        FontName: 'Secondary',
+                        RenderUppercase:true,
+                        RenderBold: true,
+                        Wrap: true,
+                        TextColor: colors.activeTextPrimary
+                    }"
+                >{{ objective.text }}</Label>
+        </Group>
+
+        <Group :anchor="{Top:0,Right:80, Width:700,Height:220}" :background="'Img/top_right_shine.png'"/>
+    </Group>
+
+
+
+   <!--==HOTBAR==-->
+    <Group :anchor="{Right:0}">
+        <!--image asset-->
+        <Group :anchor="{Right:-180, Top:-10, Bottom:1, Width:320}" :background="'Img/right.png'"/>
+
+        <!--hotbar-->
+        <Group :anchor="{Right:0, Top:0, Bottom:0, Width:120}" :background="contentBg" :layout-mode="'Top'">
+            <Group v-for="entry in hotbar" :anchor="{Left:0, Top:22, Width: 100, Height: 100 }" :background="entry.active ? 'Img/button_on_overlay.png' : 'Img/button_off_overlay.png'" :mask-texture-path="'Img/masks/button.png'">
+                <Group  :background="{Color:`${colors.activeTextPrimary}(0.5)`}"/>
+                <Group :background="entry.active ? 'Img/button_on_top_overlay.png' : 'Img/empty.png'"/>
+                <Label>{{ entry.name }}</Label>
+            </Group>
+        </Group>
+    </Group>
+
+
+
+    <!--==WEAPON INFO==-->
+    <Group :anchor="{  Right: -20, Bottom: -52}" >
+        <!--image asset-->
+        <Group :anchor="{Bottom:0,Right:-90, Width:800,Height:450}">
+            <Group :background="'Img/bottom_right.png'"/>
+        </Group>
+
         <!--Weapon graphic-->
-        <Group :anchor="{ Right:250, Bottom:0,Width: 400, Height: 150 }" :background="'Common/DropdownBox.png'">
+        <Group :anchor="{ Right:250, Bottom:0,Width: 400, Height: 150 }" :background="contentBg">
+        <Group :anchor="{Left:-20, Bottom:62, Width: 400, Height: 130 }" :background="{Color:colors.activeTextPrimary}" :mask-texture-path="'Img/masks/bottom_right_mask.png'"></Group>
 
         </Group>
         <!--Ammo counter-->
-        <Group :anchor="{ Width: 300, Height: 300, Right:0, Bottom: 0}" :background="'Common/DropdownBox.png'">
-            <Group :anchor="{Top:20,Left:5}">
+        <Group :anchor="{ Width: 300, Height: 300, Right:33, Bottom: 30}" :background="contentBg">
+            <Group :anchor="{Top:10,Left:40}">
                 <Label
                     :anchor="{Top:10,Left:0,Width:300,Height:200}"
                     :padding="{Full:20}"
                     :el-style="{
-                        FontSize: 210,
+                        FontSize: 170,
                         FontName: 'Default',
                         HorizontalAlignment: 'Start',
                         VerticalAlignment: 'Center',
@@ -148,10 +241,10 @@ function damageFlicker(){
                     }"
                 >{{ weapon.ammo }}</Label>
                 <Label
-                    :anchor="{Top:100,Left:80,Width:200,Height:200}"
+                    :anchor="{Top:100,Left:10,Width:200,Height:200}"
                     :padding="{Full:20}"
                     :el-style="{
-                        FontSize: 60,
+                        FontSize: 50,
                         FontName: 'Default',
                         HorizontalAlignment: 'End',
                         VerticalAlignment: 'Center',
@@ -160,24 +253,9 @@ function damageFlicker(){
                     }"
                 >{{ weapon.maxAmmo }}</Label>
             </Group>
-
         </Group>
-    </Group>
 
-    <!--Objectives-->
-    <Group :anchor="{Top: -20,Right:-20, Width:800, Height:120}" :background="'Common/DropdownBox.png'">
-            <Label
-                :anchor="{Top:20, Width:600, Left: 20}"
-                :padding="{Full:20}"
-                :el-style="{
-                    FontSize: 43,
-                    FontName: 'Secondary',
-                    RenderUppercase:true,
-                    RenderBold: true,
-                    Wrap: true,
-                    TextColor: colors.activeTextPrimary
-                }"
-            >{{ objective.text }}</Label>
+        <Group :anchor="{Bottom:0,Right:-90, Width:800,Height:450}" :background="'Img/bottom_right_shine.png'"/>
     </Group>
 
     <!--All-purpose overlay-->
